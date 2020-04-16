7feb2de4370ff16910e0de7c1b72fb1e6ac503ae
FORMULA1031 <56063659+FORMULA1031@users.noreply.github.com>
2020-04-17 00:34:58 +0900

    A~Cを表示する

---
+++ chouonpasenser.ino
@@ -0,0 +1,31 @@
+#define echoPin 2
+#define trigPin 3
+
+double Duration = 0;
+double Distance = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(echoPin,INPUT);
+  pinMode(trigPin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+  Duration = pulseIn(echoPin,HIGH);
+  if(Duration > 0){
+    Duration = Duration/2;
+    Distance = Duration*340*100/1000000;
+    Serial.print("Distance:");
+    Serial.print(Distance);
+    Serial.print("cm");
+    Serial.print("\n");
+  }
+  delay(500);
+}
